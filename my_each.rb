d4525cb869391b98ae75855ff8fb0cadf440c5ed
Stef Spears <spears.sj87@gmail.com>
2019-07-31 02:48:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array)
   if block_given?
     i = 0 
     while i < array.length 
-      yield array[i]
+      yield (array[i])
     end
     array
   else puts "Please provide a block, friend."
