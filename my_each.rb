9fa3832d6eff0f9d6cfb7b126abcb0367538f6ca
Stef Spears <spears.sj87@gmail.com>
2019-07-31 02:48:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
 def my_each(array)
   if block_given?
-  yield
-  array
-else puts ""
+    i = 0 
+    while i < array.length 
+      yield array[i]
+    end
+    array
+  else puts "Please provide a block, friend."
+  end
 end
